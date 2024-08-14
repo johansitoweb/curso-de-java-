e5dfd689d18c039d02f07537d1b2e5d447c57b13
Johan  Antonio <147890607+johansitoweb@users.noreply.github.com>
2024-08-14 18:06:47 +0100

    Add files via upload

---
+++ areaRectangulo.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("El area de un rectanglo");
+        int altura = 15;
+        int ancho = 10;
+        int area;
+        System.out.println(altura * ancho + ": esta es el area del rctangulo"); // entonces al mustiplicar estos dos me da como resultado que el area
+    }
+}
+
+
